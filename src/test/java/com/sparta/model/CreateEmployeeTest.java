063036d6672dd51f23ede5905fd1768923c2e523
BahadirUK <100964237+BnBuk@users.noreply.github.com>
2022-09-26 10:03:10 +0100

    )

--- src/test/java/com/sparta/model/CreateEmployeeTest.java
+++ src/test/java/com/sparta/model/CreateEmployeeTest.java
@@ -1,7 +1,11 @@
 package com.sparta.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import static com.sparta.model.CreateEmployee.createEmp;
 import static com.sparta.model.JavaFaker.getJavaFaker;
@@ -10,22 +14,28 @@ class CreateEmployeeTest {
 
 
     @Test
-    @DisplayName("Test")
-    void createEmpTest() {
-        int employeeID = -12345;
-        String prefix = "Mr";
-        String firstName = "J0hn";
-        char middleInitial = 'Ω';
-        String lastName = "";
-        char gender = 'Z';
-        String mail = getJavaFaker().internet().emailAddress();
-        java.util.Date dob = getJavaFaker().date().birthday();
+    @DisplayName("Testing employee creating method with valid inputs")
+    void createEmpTest() throws ParseException {
+        int employeeID = getJavaFaker().number().numberBetween(100_000, 1_000_000);
+        String prefix = "Mr.";
+        String firstName = getJavaFaker().name().firstName();
+        char middleInitial = getJavaFaker().name().nameWithMiddle().charAt(0);
+        String lastName = getJavaFaker().name().lastName();
+        char gender = 'F';
+        String mail = getJavaFaker().internet().emailAddress(firstName + "." + middleInitial + "." + lastName);
+        String dob = "11/16/1997";
+        String employmentDate = "12/29/2019";
+        int salary = 12340;
 
-        java.util.Date employmentDate = getJavaFaker().date().birthday();
-        int salary = -123456;
 
-        //createEmp(employeeID,prefix,firstName,middleInitial,lastName,gender,mail,dob,employmentDate,salary);
+        String line = employeeID + "," + prefix + "," + firstName + "," + middleInitial + "," + lastName + "," + gender + "," + mail + "," + dob + "," + employmentDate + "," + salary;
+        Employee newEmp = createEmp(line);
 
+            Assertions.assertEquals(employeeID, newEmp.getEmployeeID());
+            Assertions.assertEquals(firstName, newEmp.getFirstName());
+            Assertions.assertEquals(prefix, newEmp.getPrefix());
+            Assertions.assertEquals(new SimpleDateFormat("MM/dd/yyyy").parse(dob), newEmp.getDob());
+            Assertions.assertEquals(middleInitial, newEmp.getMiddleInitial());
 
     }
 }
---
+++ src/test/java/com/sparta/model/EmployeeTest.java
@@ -0,0 +1,6 @@
+package com.sparta.model;
+
+public class EmployeeTest {
+
+
+}
